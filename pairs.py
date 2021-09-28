9ce15cd770a3a9eaac7f917f2ab2c04692909bfb
ysdede <yunusseyhandede@gmail.com>
2021-09-28 22:28:40 +0300

    Initial

---
+++ pairs.py
@@ -0,0 +1,15 @@
+# FTX Perpetual Futures symbols sorted by daily volume @23-09-2021
+ftx_perp_pairs = [
+    "BTC-USD",
+    "ETH-USD",
+    "SOL-USD",
+    "AVAX-USD",
+]
+
+# Binance Perpetual Futures symbols sorted by volume @27/09/2021
+binance_perp_pairs = [
+    "BTC-USDT",
+    "ETH-USDT",
+    "SOL-USDT",
+    "DYDX-USDT",
+]
